3eb883a2ecb70a397fcb7bd29b393b92f8bb05d8
helena <seifert.helena@icloud.com>
2026-07-30 12:53:08 +0200

    neu anfangen

--- script.js
+++
@@ -1,7 +0,0 @@
-function rechner() {
-    let dieter = +document.getElementById("zahl1").value;
-    let svenja = +document.getElementById("zahl2").value;
-
-    let ergebnis = dieter + svenja; 
-    document.getElementById("ergebnis").innerHTML = ergebnis;
-}
